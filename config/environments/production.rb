8e53d256a1759d49337f742eb5c02fd9010f4fe3
Andrey Spies <44011140+AndreySpies@users.noreply.github.com>
2019-03-27 17:09:13 -0300

    Update production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,6 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
-  environment 'config.action_mailer.default_url_options = { host: "http://set-domain-here" }', env: 'production'
+  environment 'config.action_mailer.default_url_options = { host: "http://set-domain-here" }'
   # Code is not reloaded between requests.
   config.cache_classes = true
 
