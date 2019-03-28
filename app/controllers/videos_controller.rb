98145c1057b5b553b503159594926fd25d1c84b7
Andrey Spies <andreyspies@gmail.com>
2019-03-28 00:10:30 -0300

    Set create method on Videos controller

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -1,4 +1,18 @@
 class VideosController < ApplicationController
+  def new
+    @video = Video.new
+  end
+
+  def create
+    @video = Video.new(video_params)
+    @video.user = current_user
+    if @video.save
+      redirect_to video_path(@video.url)
+    else
+      render :new
+    end
+  end
+
   def show
     url = "#{params[:id]}.#{params[:format]}"
     @video = Video.find_by(url: url)
@@ -11,5 +25,11 @@ class VideosController < ApplicationController
   end
 
   def destroy
+  end
+
+  private
+
+  def video_params
+    params.require(:video).permit(:name)
   end
 end
