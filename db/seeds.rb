ea1e222314cc9cfe9f35168d1bc2fc96f8c8cc34
Andrey Spies <andreyspies@gmail.com>
2019-03-28 00:05:26 -0300

    Updated seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,5 +11,6 @@ user = User.create!(email: 'user@gmail.com', password: 'senhasenha')
 puts 'Users done!'
 
 puts 'Creating video'
-video = Video.create!(user: user, name: 'big-movie')
+video = Video.new(user: user, name: 'big-movie')
+video.save!
 puts 'Video done!'
