29b59370acfd61b7495748a4f140762893b4dfde
Andrey Spies <andreyspies@gmail.com>
2019-03-28 00:06:30 -0300

    Updated validations on video creation

--- app/models/video.rb
+++ app/models/video.rb
@@ -1,16 +1,16 @@
 class Video < ApplicationRecord
   belongs_to :user
-  # extend FriendlyId
+
   after_create :set_url
 
   validates :name, format: {
-    with: /\A[\w|\-|\s]+\z/,
+    with: /\A[\w|\-|\s|\u00C0-\u00FF]+\z/,
     message: "only allows letters, numbers, underscores and spaces"
   }
 
   private
 
   def set_url
-    update(url: "#{id}-#{name}\.m3u8")
+    update(url: "#{id}-#{name.parameterize}.m3u8")
   end
 end
