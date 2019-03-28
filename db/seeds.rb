b1d9e18bb9a8ff592d60b004033b961e5faac751
Andrey Spies <andreyspies@gmail.com>
2019-03-27 22:19:56 -0300

    Added seed to create user and video

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts 'Creating user'
+user = User.create!(email: 'user@gmail.com', password: 'senhasenha')
+puts 'Users done!'
+
+puts 'Creating video'
+video = Video.create!(user: user, name: 'big-movie')
+puts 'Video done!'
