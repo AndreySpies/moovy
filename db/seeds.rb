3759ef2d675c15fe6e9bc0a80e5d7a13d42df1d8
Andrey Spies <andreyspies@gmail.com>
2019-04-01 20:39:48 -0300

    Updated seed to create 5 videos

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,6 +8,15 @@ video = Video.new(user: user, name: 'big-movie')
 video.miniature = Rails.root.join("app/assets/images/home-banner-2.jpg").open
 video.save!
 video2 = Video.new(user: user2, name: 'Little Movie')
-video2.miniature = Rails.root.join("app/assets/images/home-banner-2.jpg").open
+video2.miniature = Rails.root.join("app/assets/images/home-banner.jpg").open
 video2.save!
+video3 = Video.new(user: user, name: 'Little Movie')
+video3.miniature = Rails.root.join("app/assets/images/home-banner-2.jpg").open
+video3.save!
+video4 = Video.new(user: user2, name: 'Little Movie')
+video4.miniature = Rails.root.join("app/assets/images/home-banner.jpg").open
+video4.save!
+video5 = Video.new(user: user, name: 'Little Movie')
+video5.miniature = Rails.root.join("app/assets/images/home-banner-2.jpg").open
+video5.save!
 puts 'Video done!'
