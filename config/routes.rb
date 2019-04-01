9837490e683e86bad0ca556ddba9323fb42b5ecf
Andrey Spies <andreyspies@gmail.com>
2019-04-01 13:56:57 -0300

    Set routes to show a user's collection

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
+  get '/:user', to: 'videos#collection', as: :collection
   resources :users
   resources :videos, only: %i[new create show update destroy]
   get 'videos/edit/:url', to: 'videos#edit', as: :edit_video
