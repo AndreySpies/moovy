6ca498addccc077631a2e7e7cba8d4466f3dc022
Andrey Spies <andreyspies@gmail.com>
2019-04-01 10:39:49 -0300

    Fixed video views in mobile

--- app/assets/javascripts/videos.js
+++ app/assets/javascripts/videos.js
@@ -1,2 +1,21 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
+let first_play = true
+const video = document.getElementById("video");
+
+const videoPlay = () => {
+  if (first_play) {
+    first_play = false
+    increaseViews();
+  }
+};
+
+const increaseViews = () => {
+  const videoId = document.getElementById('video-title').getAttribute("data-video");
+  Rails.ajax({
+    url: `/videos/views/${videoId}`,
+    type: "PATCH",
+    data: `video[views]=1`,
+    dataType : "text"
+  });
+};
--- app/javascript/plugins/init_video.js
+++ app/javascript/plugins/init_video.js
@@ -1,25 +1,3 @@
-if (document.getElementById('video-title')) {
-  let first_play = true
-  const video = document.getElementById("video");
-
-  video.onplay = function() {
-    if (first_play) {
-      first_play = false
-      increaseViews();
-    }
-  };
-}
-
-const increaseViews = () => {
-  const videoId = document.getElementById('video-title').getAttribute("data-video");
-  Rails.ajax({
-    url: `/videos/views/${videoId}`,
-    type: "PATCH",
-    data: `video[views]=1`,
-    dataType : "text"
-  });
-};
-
 const initVideo = () => {
   if (document.getElementById('video-title')) {
     (function(window, videojs) {
