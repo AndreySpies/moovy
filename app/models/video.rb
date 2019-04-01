f4e0e01f9107d88be3f3091be6f6f280f75b21db
Andrey Spies <andreyspies@gmail.com>
2019-04-01 15:15:58 -0300

    Added 'miniature' with PhotoUploader and validate to it

--- app/models/video.rb
+++ app/models/video.rb
@@ -1,10 +1,12 @@
 class Video < ApplicationRecord
   belongs_to :user
+  mount_uploader :miniature, PhotoUploader
   after_create :set_url
   validates :name, format: {
     with: /\A[\w|\-|\s|\u00C0-\u00FF]+\z/,
     message: "only allows letters, numbers, underscores, dashes and spaces"
   }
+  validates :miniature, presence: true
 
   private
 
