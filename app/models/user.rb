ad3c2c6508ec030925e088cd26610e5cdcab0149
Andrey Spies <andreyspies@gmail.com>
2019-04-02 19:16:18 -0300

    Add validation to ensure display_name is unique

--- app/models/user.rb
+++ app/models/user.rb
@@ -16,8 +16,10 @@ class User < ApplicationRecord
     with: /\A[\w|\s]+\z/,
     message: 'only allow letters and spaces'
   }
-  validates :display_name, format: {
-    with: /\A[\w|\-|\s|\u00C0-\u00FF]+\z/,
-    message: "only allows letters, numbers, underscores, dashes and spaces"
-  }
+  validates :display_name,
+            uniqueness: true,
+            format: {
+              with: /\A[\w|\-|\s|\u00C0-\u00FF]+\z/,
+              message: "only allows letters, numbers, underscores, dashes and spaces"
+            }
 end
