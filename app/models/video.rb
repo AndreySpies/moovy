c1eabae185f2a323e702014ad627dd09923e79a1
Andrey Spies <andreyspies@gmail.com>
2019-04-03 01:34:14 -0300

    Fixed video editin

--- app/models/video.rb
+++ app/models/video.rb
@@ -2,11 +2,12 @@ class Video < ApplicationRecord
   belongs_to :user
   mount_uploader :miniature, PhotoUploader
   after_create :set_url
+
   validates :name, format: {
     with: /\A[\w|\-|\s|\u00C0-\u00FF]+\z/,
     message: "only allows letters, numbers, underscores, dashes and spaces"
   }
-  validates :miniature, presence: true
+  validates :miniature, presence: true, on: :create
 
   private
 
